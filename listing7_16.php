132f55a327801fe5c74c712c2c9e2495b7a28dc7
Ashish Patel <ashpat86@gmail.com>
2017-04-17 17:52:07 -0400

    updated files

---
+++ listing7_16.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Listing 7-16</title>
+		<script type="text/javascript"></script>
+
+	</head>
+	<body onload = "doocument.regform.fullname.focus();">
+		<h2>Place Focus</h2>
+		<form name="regform">
+			<fieldset>
+				<legend>Registration Form</legend>
+				Name: <input type="text" name="fullname" size="24" />&nbsp&nbsp
+				Email: <input type="text" name="email" /><br>
+				
+			</fieldset>
+
+	</body>
+</html>
